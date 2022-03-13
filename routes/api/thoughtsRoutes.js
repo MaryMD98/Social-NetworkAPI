28021d7fbc95d564c6db814514bc3f7fa7b09c1b
Maribel Montes <maribel.doris@gmail.com>
2022-03-12 19:29:22 -0600

    initialized the thoughts routes

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+
+// GET to get all thoughts
+// POST to create a new thought 
+//(don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
+router.route('/').get().post();
+
+// GET to get a single thought by its `_id`
+// PUT to update a thought by its `_id`
+// DELETE to remove a thought by its `_id`
+router.route('/:thoughtId').get().put().delete();
+
+
+// /api/thoughts/:thoughtId/reactions
+// POST to create a reaction stored in a single thought's `reactions` array field
+// DELETE to pull and remove a reaction by the reaction's `reactionId` value
+router.route('/:thoughtId/reactions').post().delete();
