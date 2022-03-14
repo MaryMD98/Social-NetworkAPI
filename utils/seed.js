feb9ffa1777ab76f1f33d6604ffbfe5e9ec4e608
Maribel Montes <maribel.doris@gmail.com>
2022-03-14 04:10:24 -0500

    added the seed.js to seed data

--- utils/seed.js
+++ utils/seed.js
@@ -1,4 +1,19 @@
-const connection = require('../config/connection');
+const db = require('../config/connection');
 const { Thought, User } = require('../models');
 // import function to seed data
-const {} = require('./data');
+const thoughtSeeds = require('./thoughtsSeed.json');
+const userSeeds = require('./userSeed.json');
+
+db.once('open', async () => {
+    // Drop existing Thought and user charts
+    await Thought.deleteMany({});
+    await User.deleteMany({});
+
+    const thoughts = await Thought.create(thoughtSeeds);
+    const users = await User.create(userSeeds);
+
+    console.table(thoughts);
+    console.table(users);
+    console.log('all done! Seeding complete! 🌱');
+    process.exit(0);
+});
